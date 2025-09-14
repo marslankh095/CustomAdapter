d12f36392a8836e4e49b764086690ccaa9337e6b
marslankh095 <marslankh095@gmail.com>
2025-09-15 02:49:44 +0500

    f commit

--- app/src/main/java/com/app/customadapter/MainActivity2.kt
+++ app/src/main/java/com/app/customadapter/MainActivity2.kt
@@ -2,21 +2,40 @@ package com.app.customadapter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-
+import android.widget.ImageButton
+import android.widget.ImageView
 class MainActivity2 : AppCompatActivity() {
-    private val texts = arrayOf("Special Noodles","Veg Biryani","Veg Sandwich","Pizza Friday","Pancakes","Ice Creams","Veg Burger")
-    private val desc = arrayOf("non veg","pure veg","pure veg","customizable","pure veg","pure veg","pure veg")
-    private val img = arrayOf(R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e
-        ,R.drawable.f,R.drawable.g)
+
+    private var currentImage = 0
+    private lateinit var image: ImageView
+
+    // All images from drawable
+    private val images = arrayOf(
+        R.drawable.a2,
+        R.drawable.a45,
+        R.drawable.a2,
+        R.drawable.a39 
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = CustomAdapter(img,texts,desc)
+        image = findViewById(R.id.imageView)
+
+        val next = findViewById<ImageButton>(R.id.btnNext)
+        val prev = findViewById<ImageButton>(R.id.btnPrev)
+
+        image.setImageResource(images[currentImage])
+
+        next.setOnClickListener {
+            currentImage = (currentImage + 1) % images.size
+            image.setImageResource(images[currentImage])
+        }
+
+        prev.setOnClickListener {
+            currentImage = if (currentImage - 1 < 0) images.size - 1 else currentImage - 1
+            image.setImageResource(images[currentImage])
+        }
     }
 }
